1862a2c50cf646246e7a88a72b08ae54f7a7115c
Matt Menzenski <matt@mobilgov.com>
2017-08-14 19:47:06 -0500

    add a test that calls subprocess

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,12 @@
-def test_create_file(tmpdir):
+import subprocess
+
+
+def test_using_subprocess_check(tmpdir):
     p = tmpdir.mkdir("sub").join("hello.txt")
     p.write("content")
+    subprocess.check_call([
+        'cd',
+        tmpdir.strpath,
+        ])
     assert p.read() == "content"
     assert len(tmpdir.listdir()) == 1
